27efdc7ee14ec5d30b4411013191e5973dfb594d
David Bosschaert <david.bosschaert@gmail.com>
2023-07-31 17:13:47 +0100

    Initial implementation of inflate and deflate

---
+++ src/bin/main.rs
@@ -0,0 +1,30 @@
+use std::fs;
+
+use docxtools::zip_util::ZipUtil;
+
+fn main() {
+    std::process::exit(real_main());
+}
+
+fn real_main() -> i32 {
+    let args: Vec<_> = std::env::args().collect();
+    if args.len() < 3 {
+        println!("Usage: {} <filename> <tempdir>", args[0]);
+        return 1;
+    }
+
+    let src_file = &*args[1];
+    let fname = std::path::Path::new(src_file);
+    let file = fs::File::open(fname).unwrap();
+
+    let temp_dir = &*args[2];
+    let tname = std::path::Path::new(temp_dir);
+
+    ZipUtil::read_zip(file, tname);
+
+    let dest_file = src_file.to_owned() + ".xyz";
+
+    ZipUtil::write_zip(temp_dir, &dest_file).unwrap();
+
+    0
+}
---
+++ src/zip_util.rs
@@ -0,0 +1,126 @@
+use std::fs;
+use std::io;
+use std::io::{Read, Write};
+use std::path::Path;
+use zip::result::ZipError;
+use zip::write::FileOptions;
+use walkdir::{DirEntry, WalkDir};
+
+pub struct ZipUtil {
+}
+
+impl ZipUtil {
+    pub fn read_zip(file: fs::File, temp_path: &Path) {
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+
+        let outpathbase = temp_path.to_owned();
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let outpathfn = match file.enclosed_name() {
+                Some(path) => path.to_owned(),
+                None => continue,
+            };
+            
+            let outpath = outpathbase.join(outpathfn);
+
+            {
+                let comment = file.comment();
+                if !comment.is_empty() {
+                    println!("File {i} comment: {comment}");
+                }
+            }
+    
+            if (*file.name()).ends_with('/') {
+                println!("File {} extracted to \"{}\"", i, outpath.display());
+                fs::create_dir_all(&outpath).unwrap();
+            } else {
+                println!(
+                    "File {} extracted to \"{}\" ({} bytes)",
+                    i,
+                    outpath.display(),
+                    file.size()
+                );
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p).unwrap();
+                    }
+                }
+                let mut outfile = fs::File::create(&outpath).unwrap();
+                io::copy(&mut file, &mut outfile).unwrap();
+            }
+    
+            // Get and Set permissions
+            #[cfg(unix)]
+            {
+                use std::os::unix::fs::PermissionsExt;
+    
+                if let Some(mode) = file.unix_mode() {
+                    fs::set_permissions(&outpath, fs::Permissions::from_mode(mode)).unwrap();
+                }
+            }
+        }    
+    }
+
+    pub fn write_zip(
+        src_dir: &str,
+        dst_file: &str,
+    ) -> zip::result::ZipResult<()> {
+        if !Path::new(src_dir).is_dir() {
+            return Err(ZipError::FileNotFound);
+        }
+    
+        let path = Path::new(dst_file);
+        let file = fs::File::create(path).unwrap();
+    
+        let walkdir = WalkDir::new(src_dir);
+        let it = walkdir.into_iter();
+    
+        Self::deflate(&mut it.filter_map(|e| e.ok()), src_dir, file, 
+            zip::CompressionMethod::Deflated)?;
+    
+        Ok(())
+    }
+
+    fn deflate<T>(
+        it: &mut dyn Iterator<Item = DirEntry>,
+        prefix: &str,
+        writer: T,
+        method: zip::CompressionMethod,
+    ) -> zip::result::ZipResult<()>
+    where
+        T: io::Write + io::Seek,
+    {
+        let mut zip = zip::ZipWriter::new(writer);
+        let options = FileOptions::default()
+            .compression_method(method)
+            .unix_permissions(0o755);
+    
+        let mut buffer = Vec::new();
+        for entry in it {
+            let path = entry.path();
+            let name = path.strip_prefix(Path::new(prefix)).unwrap();
+    
+            // Write file or directory explicitly
+            // Some unzip tools unzip files with directory paths correctly, some do not!
+            if path.is_file() {
+                println!("adding file {path:?} as {name:?} ...");
+                #[allow(deprecated)]
+                zip.start_file_from_path(name, options)?;
+                let mut f = fs::File::open(path)?;
+    
+                f.read_to_end(&mut buffer)?;
+                zip.write_all(&buffer)?;
+                buffer.clear();
+            } else if !name.as_os_str().is_empty() {
+                // Only if not root! Avoids path spec / warning
+                // and mapname conversion failed error on unzip
+                println!("adding dir {path:?} as {name:?} ...");
+                #[allow(deprecated)]
+                zip.add_directory_from_path(name, options)?;
+            }
+        }
+        zip.finish()?;
+        Result::Ok(())
+    }    
+}
