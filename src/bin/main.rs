30dbdefce6b86a3324469f3b69c1be2a80b11293
David Bosschaert <david.bosschaert@gmail.com>
2023-08-01 16:52:58 +0100

    Add clap

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,28 @@
 use std::fs;
+use clap::Parser;
 
 use docxtools::xml_util::XMLUtil;
 use docxtools::zip_util::ZipUtil;
 
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
+
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
 fn main() {
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
+    
     std::process::exit(real_main());
 }
 
