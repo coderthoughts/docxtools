2becb634398c239713de4add98c6d40dcf4dedb6
David Bosschaert <david.bosschaert@gmail.com>
2023-08-01 14:25:25 +0100

    Change some XML

--- src/xml_util.rs
+++ src/xml_util.rs
@@ -1,15 +1,49 @@
 use xml_dom::parser::read_xml;
+use xml_dom::level2::*;
+// use xml_dom::shared::rc_cell::RcRefCell;
+// use xml_dom::level2::NodeImpl;
+// use xml_dom::level2::NodeImpl;
+// use xml_dom::level2::RcRefCell;
 
 pub struct XMLUtil {
 }
 
 impl XMLUtil {
+    fn read_node(node: &mut RefNode) {
+        for mut r in node.child_nodes() {
+            let val = r.value();
+            println!("Found child {:?}", val);
+
+            Self::read_node(&mut r);
+
+            let _ = match val {
+                Some(s) => { 
+                    println!("Val: {}", s);
+                    if s.eq_ignore_ascii_case("ya") {
+                        let _ = r.set_value("RRR"); 
+                        println!("Changed: {}", s);     
+                    }
+                } ,
+                None => (),
+            };
+
+        }
+    }
+
     pub fn read_xmls(root_dir: &str) {
 
-        let dom = read_xml(r#"<?xml version="1.0"?>
-        <hi>hihih</hi>
-        "#);
+        let mut dom = read_xml(r#"<?xml version="1.0"?>
+        <hi>hihih<yo>ya</yo></hi>
+        "#).unwrap();
         // dom.
-        println!("Read dom: {:?}", dom);
+        // println!("Read dom: {:?}", dom);
+
+        Self::read_node(&mut dom);
+        Self::read_node(&mut dom);
+
+        // for r in dom.child_nodes() {
+        //     println!("Found child {:?}", r);
+        //     // r.child_nodes()
+        // }
     }
 }
