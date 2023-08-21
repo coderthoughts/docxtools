1bc2a379d6ea4a19e7ece2def82b267f6e94847e
David Bosschaert <bosschae@adobe.com>
2023-08-21 15:30:51 +0100

    Handle file BOMs

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,23 +3,14 @@ use clap::Parser;
 use docxtools::xml_util::XMLUtil;
 use docxtools::zip_util::ZipUtil;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    // /// Name of the person to greet
-    // #[arg(short, long)]
-    // name: String,
-
-    // /// Number of times to greet
-    // #[arg(short, long, default_value_t = 1)]
-    // count: u8,
-    
     #[arg(long)]
     command: String,
 
     #[arg(long)]
-    grep_pattern: String,
+    grep_pattern: Option<String>,
 
     #[arg(short, long)]
     in_file: String,
@@ -27,16 +18,13 @@ struct Args {
     // #[arg(short, long)]
     // out_file: String,
 
+    // TODO automatically pick a temp dir
     #[arg(short, long)]
     temp_dir: String
 }
 fn main() {
     let args = Args::parse();
 
-    // for _ in 0..args.count {
-    //     println!("Hello {}!", args.name)
-    // }
-    
     std::process::exit(real_main(args));
 }
 
@@ -55,8 +43,11 @@ fn real_main(args: Args) -> i32 {
     ZipUtil::read_zip(&src_file, &temp_dir).unwrap();
 
     match args.command.as_str() {
+        "cat" => {
+            XMLUtil::cat(&temp_dir);
+        },
         "grep" => {
-            XMLUtil::grep_xml(&temp_dir, &args.grep_pattern)
+            XMLUtil::grep_xml(&temp_dir, &args.grep_pattern.unwrap())
         },
         _ => panic!("Unknown command {}", args.command)
     }
@@ -64,5 +55,6 @@ fn real_main(args: Args) -> i32 {
 
     // ZipUtil::write_zip(&temp_dir, &dest_file).unwrap();
 
+    // TODO delete temp dir
     0
 }
--- src/xml_util.rs
+++ src/xml_util.rs
@@ -1,53 +1,75 @@
 use regex::Regex;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader,Read};
 use std::path::Path;
 use xml_dom::parser::{read_xml, read_reader};
 use xml_dom::level2::*;
+use unicode_bom::Bom;
 use walkdir::WalkDir;
-// use xml_dom::shared::rc_cell::RcRefCell;
-// use xml_dom::level2::NodeImpl;
-// use xml_dom::level2::NodeImpl;
-// use xml_dom::level2::RcRefCell;
 
 pub struct XMLUtil {
 }
 
 impl XMLUtil {
+    pub fn cat(dir: &str) {
+        Self::grep_xml(dir, "");
+    }
+
     pub fn grep_xml(dir: &str, pattern: &str) {
-        let regex = Regex::new(pattern).unwrap();
+        let regex;
+        if pattern.len() > 0 {
+            regex = Some(Regex::new(pattern).unwrap());
+        } else {
+            regex = None;
+        }
 
         for entry in WalkDir::new(dir).into_iter().filter_map(|e| e.ok()) {
-            // println!("{}", entry.path().display());
-            if entry.file_type().is_file() {
+            if entry.file_type().is_file() && entry.file_name().to_string_lossy().ends_with(".xml") {
                 Self::grep_xml_file(entry.path(), &regex);
             }
         }
-        // let r = "";
-        // let f = File::open(dir)
-        // read_reader(r);
     }
 
-    fn grep_xml_file(path: &Path, regex: &Regex) {
+    fn grep_xml_file(path: &Path, regex: &Option<Regex>) {
+        // detect BOM
+        let bom = Self::getbom(path);
         let f = File::open(path).unwrap(); // TODO
-        let r = BufReader::new(f);
-        let dom = read_reader(r).unwrap();
+        let mut r = BufReader::new(f);
 
-        Self::grep_xml_node(&dom, regex);
+        if bom != Bom::Null {
+            // Remove the BOM bytes from the stream as they will cause the XML parsing to fail
+            let len = bom.len();
+            let mut bom_prefix = vec![0; len];
+            let _ = r.read_exact(&mut bom_prefix);
+        }
 
+        let dom_res = read_reader(r);
+
+        match dom_res {
+            Ok(dom) => Self::grep_xml_node(&dom, regex, path),
+            Err(e) => println!("Problem with XML file {}: {}", path.display(), e)
+        }
     }
 
-    fn grep_xml_node(node: &RefNode, regex: &Regex) {
-
+    fn grep_xml_node(node: &RefNode, regex: &Option<Regex>, path: &Path) {
         for n in node.child_nodes() {
             if let Option::Some(v) = n.node_value() {
-                println!("{:?}", v);
+                if v.len() == 0 {
+                    continue;
+                }
 
-                if regex.is_match(&v) {
-                    println!("Matches ^^^");
+                match regex {
+                    Some(r) => {
+                        if r.is_match(&v) {
+                            println!("{}: {}", path.to_string_lossy(), v);
+                        }
+                    },
+                    None => {
+                        println!("{}: {}", path.to_string_lossy(), v);
+                    }
                 }
             }
-            Self::grep_xml_node(&n, regex);
+            Self::grep_xml_node(&n, regex, path);
         }
     }
 
@@ -62,8 +84,8 @@ impl XMLUtil {
                 Some(s) => {
                     println!("Val: {}", s);
                     if s.eq_ignore_ascii_case("ya") {
-                        let _ = r.set_node_value("RRR"); 
-                        println!("Changed: {}", s);     
+                        let _ = r.set_node_value("RRR");
+                        println!("Changed: {}", s);
                     }
                     if s.eq_ignore_ascii_case("hihih") {
                         let cl = r.child_nodes();
@@ -88,5 +110,10 @@ impl XMLUtil {
         Self::read_node(&mut dom);
 
         println!("Resulting XML: {}", dom.to_string());
+    }
+
+    fn getbom(path: &Path) -> Bom {
+        let mut file = File::open(path).unwrap();
+        Bom::from(&mut file)
     }
 }
