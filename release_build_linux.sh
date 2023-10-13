1c5753e5e30b96d959ca022d7448b6b8fc5f5867
David Bosschaert <david.bosschaert@gmail.com>
2023-10-13 15:05:08 +0100

    Avoid full local paths to library dependencies in panic messages.

--- release_build_linux.sh
+++ release_build_linux.sh
@@ -1,4 +1,9 @@
 #!/bin/bash +x
 cargo clean
-RUSTFLAGS='-C target-feature=+crt-static' cargo build --release --target x86_64-unknown-linux-gnu
+
+# The remap-path-prefix is to avoid full paths to dependency source files in case any
+# code in there panics.
+# The crt-static is to statically link dependencies which makes the linux executable 
+# more portable.
+RUSTFLAGS="-C target-feature=+crt-static --remap-path-prefix $HOME/.cargo/registry/=./" cargo build --release --target x86_64-unknown-linux-gnu
 echo The release can be found in ./target/x86_64-unknown-linux-gnu/release/docxtools
