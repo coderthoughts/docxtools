7707eb4a87093f0c6aead1115257490fb0be9a0f
Darin Kuntze <dkuntze@gmail.com>
2025-02-17 01:32:44 -0800

    support checking if doc changed or not and NOT touching the zip (#23)

--- src/zip_util.rs
+++ src/zip_util.rs
@@ -1,9 +1,9 @@
 use std::fs;
-use std::io;
-use std::io::{Read, Write};
+use std::io::{self, Read, Write, Seek};
 use std::path::Path;
+use std::io::Cursor;
 use zip::result::ZipError;
-use zip::write::FileOptions;
+use zip::write::SimpleFileOptions;
 use walkdir::{DirEntry, WalkDir};
 
 pub struct ZipUtil {
@@ -88,13 +88,28 @@ impl ZipUtil {
         }
 
         let path = Path::new(dst_file);
-        let file = fs::File::create(path)?;
+        let mut new_zip_data = Vec::new();
+        let mut cursor = Cursor::new(&mut new_zip_data);
 
         let walkdir = WalkDir::new(src_dir);
         let it = walkdir.into_iter();
 
-        Self::deflate(&mut it.filter_map(|e| e.ok()), src_dir, file,
-            zip::CompressionMethod::Deflated)?;
+        Self::deflate(&mut it.filter_map(|e| e.ok()), src_dir, &mut cursor,
+                      zip::CompressionMethod::Deflated)?;
+
+        if path.exists() {
+            let mut existing_zip_data = Vec::new();
+            let mut existing_file = fs::File::open(path)?;
+            existing_file.read_to_end(&mut existing_zip_data)?;
+
+            if new_zip_data == existing_zip_data {
+                // No changes, skip writing
+                return Ok(());
+            }
+        }
+
+        let mut file = fs::File::create(path)?;
+        file.write_all(&new_zip_data)?;
 
         Ok(())
     }
@@ -106,23 +121,17 @@ impl ZipUtil {
         method: zip::CompressionMethod,
     ) -> zip::result::ZipResult<()>
     where
-        T: io::Write + io::Seek,
+        T: Write + Seek,
     {
         let mut zip = zip::ZipWriter::new(writer);
-        let options = FileOptions::default()
-            .compression_method(method)
-            .unix_permissions(0o755);
+        let options = SimpleFileOptions::default().compression_method(method).unix_permissions(0o755);
 
         let mut buffer = Vec::new();
         for entry in it {
             let path = entry.path();
             let name = path.strip_prefix(Path::new(prefix)).unwrap();
 
-            // Write file or directory explicitly
-            // Some unzip tools unzip files with directory paths correctly, some do not!
             if path.is_file() {
-                // println!("adding file {path:?} as {name:?} ...");
-                #[allow(deprecated)]
                 zip.start_file_from_path(name, options)?;
                 let mut f = fs::File::open(path)?;
 
@@ -130,15 +139,11 @@ impl ZipUtil {
                 zip.write_all(&buffer)?;
                 buffer.clear();
             } else if !name.as_os_str().is_empty() {
-                // Only if not root! Avoids path spec / warning
-                // and mapname conversion failed error on unzip
-                // println!("adding dir {path:?} as {name:?} ...");
-                #[allow(deprecated)]
                 zip.add_directory_from_path(name, options)?;
             }
         }
         zip.finish()?;
-        Result::Ok(())
+        Ok(())
     }
 }
 
